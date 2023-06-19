cad3d5f1b26b76a955b4d15cfa85bba5c6db2b02
kano <steen4145@gmail.com>
2023-06-19 12:22:46 +0200

    change tabgroup name and default value

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,16 @@
 <script>
     import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 
-    let tabSet = 2;
+    let tabSet = 0;
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 
     <section>
         <TabGroup >
-            <Tab bind:group={tabSet} name="tab1" value={0}>Mensa 1</Tab>
-            <Tab bind:group={tabSet} name="tab2" value={1}>Mensa 2</Tab>
-            <Tab bind:group={tabSet} name="tab3" value={2}>360°</Tab>
+            <Tab bind:group={tabSet} name="Mensa1" value={0}>Mensa 1</Tab>
+            <Tab bind:group={tabSet} name="Mensa2" value={1}>Mensa 2</Tab>
+            <Tab bind:group={tabSet} name="360°" value={2}>360°</Tab>
             <!-- Tab Panels --->
             <svelte:fragment slot="panel">
                 {#if tabSet === 0}
